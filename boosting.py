70e9f6eecfb9d2fcb1e6cc670cb08fd31fccde3a
iam10 <mcian91@hotmail.com>
2017-05-11 13:04:13 -0600

    boosting and machine_learning now run and have example code

--- boosting.py
+++ boosting.py
@@ -1,42 +1,75 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
 from sklearn.ensemble import RandomForestRegressor
 from sklearn.ensemble import GradientBoostingRegressor
 from sklearn.ensemble import AdaBoostRegressor
 from sklearn.model_selection import train_test_split, cross_val_score
 from sklearn.model_selection import GridSearchCV
+from future.utils import iteritems
+from sklearn.pipeline import Pipeline
+from sklearn.preprocessing import StandardScaler
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import mean_squared_error
+
+from sklearn.datasets import load_boston
 
 
 
 # Boosting
-def run_RandomForestRegressor(n_estimators=10,  criterion='mse', max_depth=None, oob_score=False, n_jobs=-1, random_state=None):
-    rfr = RandomForestRegressor(n_estimators=n_estimators,  criterion=criterion, max_depth=max_depth, oob_score=oob_score, n_jobs=n_jobs, random_state=random_state)
-    # X and y weren't passed in
-    rfr.fit(X, y)
-    if X_test != None:
-        return rfr, rfr.predict(X_test)
-    else:
-        return rfr
 
-def run_GradientBoostingRegressor(learning_rate=0.1, loss='ls', n_estimators=100, random_state=None):
-    gbr = GradientBoostingRegressor(learning_rate=learning_rate, loss=loss, n_estimators=n_estimators, random_state=random_state)
-    gbr.fit(X, y)
-    if X_test != None:
-        return gbr, gbr.predict(X_test)
-    else:
-        return gbr
+# A bunch of these functions are super repeditive
+# This is much more abstract and replaces a lot of these
+def run_model(cls, X, y, X_test=None, scale=False, **kwargs):
+    # All kwargs are all passed along to SVC object
+    model_args = {'model__{}'.format(k): v for k, v in iteritems(kwargs)}
 
-def run_AdaBoostRegressor(estimator, learning_rate=1.0, loss='linear', n_estimators=50, random_state=None):
-    abr = AdaBoostRegressor(estimator, learning_rate=learning_rate, loss=loss, n_estimators=n_estimators, random_state=random_state)
-    gnb.fit(X, y)
-    if X_test != None:
-        return gnb, gnb.predict(X_test)
+    if scale:
+        model = Pipeline([
+            ('scaler', StandardScaler()),
+            ('model', cls())
+        ])
     else:
-        return gnb
+        model = Pipeline([('model', cls())])
+
+    # Pass all given args to the specified model
+    model.set_params(**model_args)
+    # Fit the model to the given training data
+    model.fit(X, y)
+
+    if X_test is not None:
+        return model, model.predict(X_test)
+    else:
+        return model
 
 def run_cross_val_score(estimator, X, y=None, scoring='neg_mean_squared_error', n_jobs=-1):
     return cross_val_score(estimator, X, y=y, scoring=scoring, n_jobs=n_jobs)
 
-def run_GridSearchCV(estimator, param_grid, scoring='neg_mean_squared_error', n_jobs=-1, verbose=True):
-    rf_gridsearch = GridSearchCV(estimator, param_grid, n_jobs=n_jobs, verbose=verbose, scoring=scoring)
+def gridsearch_with_output(estimator, parameter_grid, X_train, y_train):
+    '''
+        Parameters: estimator: the type of model (e.g. RandomForestRegressor())
+                    paramter_grid: dictionary defining the gridsearch parameters
+                    X_train: 2d numpy array
+                    y_train: 1d numpy array
+
+        Returns:  best parameters and model fit with those parameters
+    '''
+    model_gridsearch = GridSearchCV(estimator,
+                                    parameter_grid,
+                                    n_jobs=-1,
+                                    verbose=True,
+                                    scoring='neg_mean_squared_error')
+    model_gridsearch.fit(X_train, y_train)
+    best_params = model_gridsearch.best_params_
+    model_best = model_gridsearch.best_estimator_
+    print("\nResult of gridsearch:")
+    print("{0:<20s} | {1:<8s} | {2}".format("Parameter", "Optimal", "Gridsearch values"))
+    print("-" * 55)
+    for param, vals in iteritems(parameter_grid):
+        print("{0:<20s} | {1:<8s} | {2}".format(str(param),
+                                                str(best_params[param]),
+                                                str(vals)))
+    return best_params, model_best
 
 def stage_score_plot(estimator, X_train, y_train, X_test, y_test):
     '''
@@ -58,3 +91,62 @@ def stage_score_plot(estimator, X_train, y_train, X_test, y_test):
         mse_test.append(mean_squared_error(y_test,list(y_predict[i,:])))
     plt.plot(mse_train,'--',label=estimator.__class__.__name__+' Train - learning rate '+str(estimator.learning_rate))
     plt.plot(mse_test,label=estimator.__class__.__name__+' Test - learning rate '+str(estimator.learning_rate))
+
+def load_and_split_data():
+    ''' Loads sklearn's boston dataset and splits it into train:test datasets
+        in a ratio of 80:20. Also sets the random_state for reproducible
+        results each time model is run.
+
+        Parameters: None
+
+        Returns:  (X_train, X_test, y_train, y_test):  tuple of numpy arrays
+                  column_names: numpy array containing the feature names
+    '''
+    boston = load_boston() #load sklearn's dataset
+    X, y = boston.data, boston.target
+    column_names = boston.feature_names
+    X_train, X_test, y_train, y_test = train_test_split(X, y,
+                                       test_size = 0.2,
+                                       random_state = 1)
+    return (X_train, X_test, y_train, y_test), column_names
+
+if __name__=='__main__':
+    # get data
+    data = np.genfromtxt('data/spam.csv', delimiter=',')
+    y = data[:, -1]
+    X = data[:, 0:-1]
+    X_train, X_test, y_train, y_test = train_test_split(X, y)
+
+    (X_train, X_test, y_train, y_test), column_names = load_and_split_data()
+
+    # example usage
+    rfr, y_predict_rfr = run_model(RandomForestRegressor, X_train, y_train, X_test=X_test, scale=False, n_estimators=10,  criterion='mse', max_depth=None, oob_score=False, n_jobs=-1, random_state=None)
+    gbr, y_predict_gbr = run_model(GradientBoostingRegressor, X_train, y_train, X_test=X_test, scale=False, learning_rate=0.1, loss='ls', n_estimators=100, random_state=None)
+    adr, y_predict_adr = run_model(AdaBoostRegressor, X_train, y_train, X_test=X_test, scale=False, base_estimator=None, learning_rate=1.0, loss='linear', n_estimators=50, random_state=None)
+
+    score_rfr = run_cross_val_score(rfr, X_train, y=y_train, scoring='neg_mean_squared_error', n_jobs=-1)
+    score_gbr = run_cross_val_score(gbr, X_train, y=y_train, scoring='neg_mean_squared_error', n_jobs=-1)
+    score_adr = run_cross_val_score(adr, X_train, y=y_train, scoring='neg_mean_squared_error', n_jobs=-1)
+
+    random_forest_grid = {'max_depth': [3, None],
+                      'max_features': ['sqrt', 'log2', None],
+                      'min_samples_split': [2, 4],
+                      'min_samples_leaf': [1, 2, 4],
+                      'bootstrap': [True, False],
+                      'n_estimators': [10, 20, 40, 80],
+                      'random_state': [1]}
+
+    gradient_boosting_grid = {'learning_rate': [0.1, 0.05, 0.02, 0.01],
+                              'max_depth': [2, 4, 6],
+                              'min_samples_leaf': [1, 2, 5, 10],
+                              'max_features': [1.0, 0.3, 0.1],
+                              'n_estimators': [500],
+                              'random_state': [1]}
+
+    rfr_best_params, rfr_best_model = gridsearch_with_output(RandomForestRegressor(), random_forest_grid, X_train, y_train)
+    gbr_best_params, gbr_best_model = gridsearch_with_output(GradientBoostingRegressor(), gradient_boosting_grid, X_train, y_train)
+
+    stage_score_plot(GradientBoostingRegressor(), X_train, y_train, X_test, y_test)
+    plt.show()
+    stage_score_plot(AdaBoostRegressor(), X_train, y_train, X_test, y_test)
+    plt.show()
--- machine_learning.py
+++ machine_learning.py
@@ -1,3 +1,8 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+from future.utils import iteritems
 import statsmodels.api as sm
 from sklearn.linear_model import LinearRegression
 from sklearn.linear_model import LogisticRegression
@@ -9,99 +14,16 @@ from sklearn.cluster import KMeans
 from sklearn.naive_bayes import GaussianNB
 from scipy.spatial.distance import pdist, squareform
 from scipy.cluster.hierarchy import linkage, dendrogram
+from sklearn.pipeline import Pipeline
+from sklearn.preprocessing import StandardScaler
+from sklearn.model_selection import train_test_split
+
+from sklearn import datasets
+
+from sklearn.feature_extraction.text import TfidfVectorizer
 
 
 # Machine Learning
-def run_OLS(X, y, X_test=None):
-    ols = sm.OLS(y_train, X_train)
-    ols.fit()
-    if X_test != None:
-        return ols, ols.predict(X_test)
-    else:
-        return ols
-
-def run_LinearRegression(X, y, X_test=None):
-    lr = LinearRegression()
-    lr.fit(X,y)
-    if X_test != None:
-        return lr, lr.predict(X_test)
-    else:
-        return lr
-
-def run_LogisticRegression(X, y, X_test=None):
-    lr = LogisticRegression()
-    lr.fit(X, y)
-    if X_test != None:
-        return lr, lr.predict(X_test)
-    else:
-        return lr
-
-def run_RandomForestClassifier(X, y, X_test=None, **kwargs):
-    rfc = RandomForestClassifier(**kwargs)
-    rfc.fit(X, y)
-    if X_test is not None:
-        return rfc, rfc.predict(X_test)
-    else:
-        return rfc
-
-def run_lSVC(X, y, X_test=None, **kwargs):
-    lSVC = LinearSVC(**kwargs)
-    lSVC.fit(X, y)
-    if X_test != None:
-        return lSVC, lSVC.predict(X_test)
-    else:
-        return lSVC
-
-def run_SVC(X, y, X_test=None, scale=True, **kwargs):
-    # All kwargs are all passed along to SVC object
-    model_args = {'model__{}'.format(k): v for k, v in iteritems(kwargs)}
-
-    if scale:
-        model = Pipeline([
-            ('scaler', StandardScaler),
-            ('model', SVC)
-        ])
-    else:
-        model = Pipeline([('model', SVC)])
-
-    model.set_params(**model_args)
-
-    model.fit(X, y)
-    if X_test is not None:
-        return model, model.predict(X_test)
-    else:
-        return model
-
-def run_KNeighborsClassifier(X, y, X_test=None, n_neighbors=5, weights='uniform', metric='minkowski', n_jobs=-1):
-    knn = KNeighborsClassifier(n_neighbors=3)
-    knn.fit(X, y)
-    if X_test != None:
-        return knn, knn.predict(X_test)
-    else:
-        return knn
-
-def run_KMeans(X, X_test=None, n_clusters=8, max_iter=300, n_jobs=-1, random_state=None):
-    km = KMeans(n_clusters=n_clusters, max_iter=max_iter, n_jobs=n_jobs, random_state=random_state)
-    km.fit(X)
-    if X_test != None:
-        return km, km.predict(X_test)
-
-def create_dentrogram(tf):
-    '''
-    Assumes its being given a tfidf matrix
-    '''
-    d = pdist(tf.todense())
-    sf = squareform(d)
-    Z = linkage(sf)
-    return dendrogram(Z)
-
-def run_GaussianNB(X, y, priors=None):
-    gnb = GaussianNB(priors=priors)
-    gnb.fit(X, y)
-    if X_test != None:
-        return gnb, gnb.predict(X_test)
-    else:
-        return gnb
 
 # A bunch of these functions are super repeditive
 # This is much more abstract and replaces a lot of these
@@ -111,11 +33,11 @@ def run_model(cls, X, y, X_test=None, scale=False, **kwargs):
 
     if scale:
         model = Pipeline([
-            ('scaler', StandardScaler),
-            ('model', cls)
+            ('scaler', StandardScaler()),
+            ('model', cls())
         ])
     else:
-        model = Pipeline([('model', cls)])
+        model = Pipeline([('model', cls())])
 
     # Pass all given args to the specified model
     model.set_params(**model_args)
@@ -127,5 +49,73 @@ def run_model(cls, X, y, X_test=None, scale=False, **kwargs):
     else:
         return model
 
-# example usage
-# run_model(RandomForestRegressor, X, y, X_test, scale=False, n_estimators=10, n_jobs=-1)
+def run_OLS(X, y):
+    ols = sm.OLS(y, X)
+    ols.fit()
+    return ols
+
+def run_KMeans(X, X_test=None, n_clusters=8, max_iter=300, n_jobs=-1, random_state=None):
+    km = KMeans(n_clusters=n_clusters, max_iter=max_iter, n_jobs=n_jobs, random_state=random_state)
+    km.fit(X)
+    if X_test is not None:
+        return km, km.predict(X_test)
+
+def create_dentrogram(df):
+    vectorizer = TfidfVectorizer(stop_words='english', max_features=1000)
+    X = vectorizer.fit_transform(df['content'])
+    features = vectorizer.get_feature_names()
+
+    # now get distances
+    distxy = squareform(pdist(X.todense(), metric='cosine'))
+
+    # 4. Pass this matrix into scipy's linkage function to compute our
+    # hierarchical clusters.
+    link = linkage(distxy, method='complete')
+
+    # 5. Using scipy's dendrogram function plot the linkages as
+    # a hierachical tree.
+
+    labels = (df['headline'] + ' :: ' + df['section_name']).values
+    dendro = dendrogram(link, color_threshold=1.5, leaf_font_size=9,
+                        labels=labels)
+    # fix spacing to better view dendrogram and the labels
+    plt.subplots_adjust(top=.99, bottom=0.5, left=0.05, right=0.99)
+    plt.show()
+
+
+if __name__=='__main__':
+    # get data
+    data = np.genfromtxt('data/spam.csv', delimiter=',')
+    y = data[:, -1]
+    X = data[:, 0:-1]
+    X_train, X_test, y_train, y_test = train_test_split(X, y)
+
+    # example usage
+    ols = run_OLS(X_train, y_train)
+    gnb, y_predict_gnb = run_model(GaussianNB, X_train, y_train, X_test=X_test, priors=None)
+    linr, y_predict_linr = run_model(LinearRegression, X_train, y_train, X_test=X_test)
+    logr, y_predict_logr = run_model(LogisticRegression, X_train, y_train, X_test=X_test)
+    rfc, y_predict_rfc = run_model(RandomForestClassifier, X_train, y_train, X_test=X_test)
+    lSVCy_predict_lSVC = run_model(LinearSVC, X_train, y_train, X_test=X_test)
+    svc, y_predict_svc = run_model(SVC, X_train, y_train, X_test=X_test, scale=True)
+    knn, y_predict_knn = run_model(KNeighborsClassifier, X_train, y_train, X_test=X_test, n_neighbors=5, weights='uniform', metric='minkowski', n_jobs=-1)
+
+    # now try KMeans and dentrogram
+    iris = datasets.load_iris()
+    X = iris.data
+    X_train, X_test = train_test_split(X)
+    km, y_predict_km = run_KMeans(X_train, X_test=X_test)
+
+    articles_df = pd.read_pickle("data/articles.pkl")
+    small_mask = np.zeros(len(articles_df)).astype(bool)
+    indices = np.arange(len(articles_df))
+    for category in articles_df['section_name'].unique():
+        category_mask = (articles_df['section_name']==category).values
+        new_index = np.random.choice(indices[category_mask])
+        small_mask[new_index] = True
+    additional_indices = np.random.choice(indices[np.logical_not(small_mask)],
+                                          100 - sum(small_mask),
+                                          replace=False)
+    small_mask[additional_indices] = True
+    small_df = articles_df.ix[small_mask]
+    create_dentrogram(small_df)
